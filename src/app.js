0bbe14ab66001b468f84060f3c6f379c7f60726f
RahulKRavi <rahulkr02042000@gmail.com>
2025-09-17 01:28:04 +0530

    feat: add mongoose schema validation in user model

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const {User} = require('./models/user')
+const { User } = require("./models/user");
 
 const connectDB = async () => {
   await mongoose.connect(
     "mongodb+srv://rahulkr02042000:WfX1LS27biU8MI5f@devtinder.niawz6c.mongodb.net/devTinder"
   );
-  console.log("Database connected")
+  console.log("Database connected");
 };
 
-connectDB().then(()=>{
-    app.listen(5000, () => {
-      console.log("Server started on port 5000");
-    })
-})
+connectDB().then(() => {
+  app.listen(5000, () => {
+    console.log("Server started on port 5000");
+  });
+});
 
-app.post('/user',async (req,res)=>{
-  const user = new User({
-    firstName: "Rahul",
-    lastName: "Ravi",
-    email: "rahul@gmail.com",
-    age: 24
-  })
-  await user.save()
-  res.send("User saved succesfully")
-})
+app.use(express.json());
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send("User saved succesfully");
+  } catch (err) {
+    res.status(401).send("Adding user got failed: " + err.message);
+  }
+});
+
+app.patch("/user:userID", async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    console.log(userID)
+    const user = await User.findByIdAndUpdate(userID, req.body);
+    await user.save()
+    res.send("User updated succesfully");
+  } catch (err) {
+    res.status(401).send("Update got failed:" + err.message);
+  }
+});
--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,47 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String
+    type: String,
+    required: true,
+    minlength: [1, 'First Name must be atlease 1 character'],
+    maxlength: [25, 'First Name can only be upto 25 charcters']
   },
   lastName: {
-    type: String
+    type: String,
+    required: true,
+    minlength: [1, 'Last Name must be atlease 1 character'],
+    maxlength: [25, 'Last Name can only be upto 25 charcters']
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    unique: true,
+    validate: function(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Email is not valid")
+      }
+    }
   },
   age: {
-    type: String
+    type: Number,
+    min: [18, 'Age should be atleast 18'],
+    max: [120, 'Enter a valide age']
+  },
+  about: {
+    type: String,
+    default: "About the user",
+    max: [250, 'Descriptino can only be upto 250 charcters']
+  },
+  photoURL: {
+    type: String,
+    default: "https://t4.ftcdn.net/jpg/12/49/12/63/360_F_1249126338_leS5yTD2NdGuTra86mGyq9heEAxLbX5O.jpg",
+    validate: function(value){
+      if(!validator.isURL(value)){
+        throw new Error("Photo URL is not valid")
+      }
+    }
   }
 })
 
